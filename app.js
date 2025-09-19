c93162e1ce0c1492cfa2b38f9b6e78a81c3f33ad
jin00100 <jin10190220@gmail.com>
2025-09-19 18:01:16 +0900

    Update to v3.0

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-	res.send('Hello from self-hosted Runner! Version 2.7,deployed automatically!')
-
-
+  res.send('Hello from Self-hosted Runner! Version 1.0');
 });
 
-app.lisen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`);
-
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
